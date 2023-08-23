a36d146fb16d3f1d2ab1ff29c3cd12f995847705
MuhammadAlamzeb <shayankhanmahar@gmail.com>
2023-08-23 21:59:32 +0500

    navbar added with router and link

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,67 +1,68 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
 const User = require("../models/userModel");
-
 const router = express.Router();
 
-//when user inputs in interface
-//creating route/api
-router.post("/", async (req,res)=> {
-    const {name,email,age} = req.body;
-    try{
+// Create User Route
+router.post("/", async (req, res) => {
+    const { name, email, age } = req.body;
+    try {
         const userAdded = await User.create({
-            name : name,
-            email : email,
-            age:age
+            name: name,
+            email: email,
+            age: age,
         });
         res.status(201).json(userAdded);
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({ error: error.message });
     }
-    catch(error){
-        console.log(error);
-        res.status(400).json({error:error.message});
-    }
-    
 });
 
-//GET API
-router.get("/", async (req,res)=>{
-    try
-    {
-        const showALL = await User.find();
-        res.status(200).json(showALL);
-        res.send("api running");
-    }catch(error){
-        console.log(error);
-        res.send(500).json({error: error.message});
+// Get All Users Route
+router.get("/", async (req, res) => {
+    try {
+        const showAll = await User.find();
+        res.status(200).json(showAll);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
     }
 });
-//GET single user by id
-router.get("/:id", async (req,res)=>{
-    const {id} = req.params;
-    try
-    {
-        const singleUser = await User.findById({_id : id});
+
+// Get Single User by ID Route
+router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const singleUser = await User.findById({ _id: id });
         res.status(200).json(singleUser);
-    }catch(error){
-        console.log(error);
-        res.send(500).json({error: error.message});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
     }
-    res.send("api running");
 });
 
-//Delete User Operation
-router.delete("/:id", async (req,res)=>{
-    const {id} = req.params;
-    try
-    {
-        const singleUser = await User.findByIdAndDelete({_id : id});
+// Delete User Route
+router.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const singleUser = await User.findByIdAndDelete({ _id: id });
         res.status(200).json(singleUser);
-    }catch(error){
-        console.log(error);
-        res.send(500).json({error: error.message});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
     }
-    res.send("api running");
 });
 
+// PUT/PATCHUPDATE
+router.patch("/:id", async (req, res) => {
+    const { id } = req.params;
+    const {name,email,age} = req.body;
+    try {
+        const updateUser = await User.findByIdAndUpdate(id,req.body,{new:true,});
+        res.status(200).json(updateUser);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+    }
+});
 module.exports = router;
