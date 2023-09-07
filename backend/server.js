2cc6306ecfd3e8a678731fd9b1ca078cc7946302
MuhammadAlamzeb <shayankhanmahar@gmail.com>
2023-09-08 00:24:23 +0500

    backend files added

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ const userRoute = require("./routes/userRoute");
 
 const app =express();
 
+const cors = require("cors");
+app.use(cors());
+
 app.use(express.json());
 
 mongoose.connect(process.env.URI).then(()=>{
